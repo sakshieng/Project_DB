8cc3eb4b40e6deff2c7041dcc86a5f969967401d
Sakshi Dhamapurkar <101258488+sakshieng@users.noreply.github.com>
2024-10-12 16:49:17 +0530

    Update connect.js

--- Backend/db/connect.js
+++ Backend/db/connect.js
@@ -1,37 +1,37 @@
-const oracledb = require('oracledb');
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+// const oracledb = require('oracledb');
+// oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-const connectoracle = async() => {
-    try {
-        const cons = await oracledb.getConnection({
-            user: "system",
-            password: "system",
-            connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))"
-        });
+// const connectoracle = async() => {
+//     try {
+//         const cons = await oracledb.getConnection({
+//             user: "system",
+//             password: "system",
+//             connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))"
+//         });
         
-        await cons.execute(`insert into client_master values('sas', 'Delhi')`);
-        await cons.execute(`insert into client_master values('sakhs', 'Delhi')`);
-        await cons.execute(`insert into client_master values('dhama', 'Mumbai')`);
+//         await cons.execute(`insert into client_master values('sas', 'Delhi')`);
+//         await cons.execute(`insert into client_master values('sakhs', 'Delhi')`);
+//         await cons.execute(`insert into client_master values('dhama', 'Mumbai')`);
 
-        const data = await cons.execute(`SELECT * FROM  client_master`);
-        console.log(data.rows);
-        console.log('Connected Successfully');
+//         const data = await cons.execute(`SELECT * FROM  client_master`);
+//         console.log(data.rows);
+//         console.log('Connected Successfully');
 
-        return cons; // Exporting the connection object
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-module.exports = connectoracle;
-
-
-// const mongoose = require(`mongoose`);
-
-// mongoose.set("strictQuery",false);
-
-// const connectDB = (url)=>{
-//     return mongoose.connect(url);
+//         return cons; // Exporting the connection object
+//     } catch (error) {
+//         console.log(error);
+//     }
 // }
 
-// module.exports = connectDB;
+// module.exports = connectoracle;
+
+
+const mongoose = require(`mongoose`);
+
+mongoose.set("strictQuery",false);
+
+const connectDB = (url)=>{
+    return mongoose.connect(url);
+}
+
+module.exports = connectDB;
